e910bfc993443e70f439d7a6d1171ab33ab39d23
ArjunBoi <arjuntaneja79@gmail.com>
2024-12-11 23:18:11 -0800

    Final commit

--- app/src/main/java/com/example/jambook/ReservationData.kt
+++ app/src/main/java/com/example/jambook/ReservationData.kt
@@ -1,12 +1,28 @@
 package com.example.jambook
 
 import androidx.compose.runtime.mutableStateListOf
+import java.time.LocalDateTime
 
 object ReservationData {
-    private val _reservations = mutableStateListOf<String>()
-    val reservations: List<String> = _reservations
+    private val _reservations = mutableStateListOf<Slot>()
+    val reservations: List<Slot> = _reservations
 
-    fun addReservation(reservation: String) {
-        _reservations.add(reservation)
+//    fun addReservation(reservation: String) {
+//        _reservations.add(reservation)
+//    }
+    fun addReservation(slot: Slot) {
+        _reservations.add(slot)
+    }
+
+    fun removeReservation(slot: Slot) {
+        _reservations.remove(slot)
+    }
+
+    fun isSlotAvailable(startTime: LocalDateTime, endTime: LocalDateTime): Boolean {
+        return _reservations.none { slot ->
+            (startTime >= slot.startTime && startTime < slot.endTime) ||
+                    (endTime > slot.startTime && endTime <= slot.endTime) ||
+                    (startTime <= slot.startTime && endTime >= slot.endTime)
+        }
     }
 }
--- app/src/main/java/com/example/jambook/ViewReservationScreen.kt
+++ app/src/main/java/com/example/jambook/ViewReservationScreen.kt
@@ -4,20 +4,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 //import com.example.jambook.ReservationData
+import com.example.jambook.Slot
+import java.time.LocalDateTime
 
 @Composable
 fun ViewReservationScreen(onBackClick: () -> Unit) {
     // Add view reservation screen content
 //    val reservationDetails = remember { mutableStateListOf<String>() }
     val reservationDetails = ReservationData.reservations
+    val UWColor = 0xFF3700B3
+
 
 //    LazyColumn(
 //        modifier = Modifier.fillMaxSize(),
@@ -33,7 +40,10 @@ fun ViewReservationScreen(onBackClick: () -> Unit) {
     ) {
         Button(
             onClick = onBackClick,
-            modifier = Modifier.align(Alignment.Start).padding(16.dp)
+            modifier = Modifier.align(Alignment.Start).padding(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(UWColor)    // Custom hex color
+            )
         ) {
             Text("Back")
         }
@@ -41,9 +51,176 @@ fun ViewReservationScreen(onBackClick: () -> Unit) {
             modifier = Modifier.fillMaxSize(),
             contentPadding = PaddingValues(16.dp)
         ) {
-            items(reservationDetails) { detail ->
-                Text(text = detail)
+            items(reservationDetails) { slot ->
+                // Display slot details
+                Column {
+                    Text("Start: ${slot.startTime}")
+                    Text("End: ${slot.endTime}")
+                    Text("User: ${slot.userName}")
+
+                    // Cancel Reservation button
+                    val confirmDialog = remember { mutableStateOf(false) }
+                    Button(
+                        onClick = {
+                            val currentTime = LocalDateTime.now()
+                            if (currentTime.isBefore(slot.startTime.minusHours(1))) {
+                                confirmDialog.value = true
+                            } else {
+                                // Show error message or disable cancel button
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(UWColor)    // Custom hex color
+                        )
+                    ) {
+                        Text("Cancel Reservation")
+                    }
+
+                    if (confirmDialog.value) {
+                        AlertDialog(
+                            onDismissRequest = { confirmDialog.value = false },
+                            title = { Text("Confirm Cancellation") },
+                            text = { Text("Are you sure you want to cancel this reservation?") },
+                            confirmButton = {
+                                Button(
+                                    onClick = {
+                                        ReservationData.removeReservation(slot)
+                                        confirmDialog.value = false
+                                    },
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = Color(UWColor)    // Custom hex color
+                                    )
+                                ) {
+                                    Text("Yes")
+                                }
+                            },
+                            dismissButton = {
+                                Button(onClick = { confirmDialog.value = false },colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(UWColor)    // Custom hex color
+                                )) {
+                                    Text("No")
+                                }
+                            }
+                        )
+                    }
+                }
             }
         }
+//        LazyColumn(
+//            modifier = Modifier.fillMaxSize(),
+//            contentPadding = PaddingValues(16.dp)
+//        ) {
+//            items(reservationDetails) { slot ->
+//                // Display slot details
+//                Row {
+//                    Text("Start: ${slot.startTime}")
+//                    Text("End: ${slot.endTime}")
+//                    Text("User: ${slot.userName}")
+//
+//                    // Cancel Reservation button
+//                    Button(
+//                        onClick = {
+//                            val currentTime = LocalDateTime.now()
+//                            if (currentTime.isBefore(slot.startTime.minusHours(1))) {
+//                                // Show confirmation dialog
+//                                val confirmDialog = remember { mutableStateOf(false) }
+//                                if (confirmDialog.value) {
+//                                    AlertDialog(
+//                                        onDismissRequest = { confirmDialog.value = false },
+//                                        title = { Text("Confirm Cancellation") },
+//                                        text = { Text("Are you sure you want to cancel this reservation?") },
+//                                        confirmButton = {
+//                                            Button(
+//                                                onClick = {
+//                                                    ReservationData.removeReservation(slot)
+//                                                    confirmDialog.value = false
+//                                                }
+//                                            ) {
+//                                                Text("Yes")
+//                                            }
+//                                        },
+//                                        dismissButton = {
+//                                            Button(onClick = { confirmDialog.value = false }) {
+//                                                Text("No")
+//                                            }
+//                                        }
+//                                    )
+//                                }
+//                            } else {
+//                                // Show error message or disable cancel button
+//                            }
+//                        }
+//                    ) {
+//                        Text("Cancel Reservation")
+//                    }
+//                }
+//            }
+//        }
+//        LazyColumn(
+//            modifier = Modifier.fillMaxSize(),
+//            contentPadding = PaddingValues(16.dp)
+//        ) {
+////            items(reservationDetails) { detail ->
+////                Text(text = detail)
+////            }
+//            items(reservationDetails) { slot ->
+//                // Display slot details
+//                Row {
+//                    Text("Start: ${slot.startTime}")
+//                    Text("End: ${slot.endTime}")
+//                    Text("User: ${slot.userName}")
+//
+//                    // Cancel Reservation button
+//                    Button(
+//                        onClick = {
+//                            val currentTime = LocalDateTime.now()
+//                            if (currentTime.isBefore(slot.startTime.minusHours(1))) {
+//                                // Show confirmation dialog
+//                                val confirmDialog = remember { mutableStateOf(false) }
+//                                if (confirmDialog.value) {
+//                                    AlertDialog(
+//                                        onDismissRequest = { confirmDialog.value = false },
+//                                        title = { Text("Confirm Cancellation") },
+//                                        text = { Text("Are you sure you want to cancel this reservation?") },
+//                                        confirmButton = {
+//                                            Button(
+//                                                onClick = {
+//                                                    ReservationData.removeReservation(slot)
+//                                                    confirmDialog.value = false
+//                                                }
+//                                            ) {
+//                                                Text("Yes")
+//                                            }
+//                                        },
+//                                        dismissButton = {
+//                                            Button(onClick = { confirmDialog.value = false }) {
+//                                                Text("No")
+//                                            }
+//                                        }
+//                                    )
+//                                }
+//
+//                                // Show cancel button
+//                                Button(onClick = { confirmDialog.value = true }) {
+//                                    Text("Cancel Reservation")
+//                                }
+//                            } else {
+//                                // Show error message or disable cancel button
+//                            }
+//                        },
+//                        content = TODO()
+////                        modifier = TODO(),
+////                        enabled = TODO(),
+////                        shape = TODO(),
+////                        colors = TODO(),
+////                        elevation = TODO(),
+////                        border = TODO(),
+////                        contentPadding = TODO(),
+////                        interactionSource = TODO(),
+////                        content = TODO()
+//                    )
+//                }
+//            }
+//        }
     }
 }
